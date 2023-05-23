b026de15e7e4a5de40d6df6558c566313c8ea498
DashaUs <dahausanova31@gmail.com>
2023-05-23 23:55:26 +0700

    master

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Lab2git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число а:");
+            double? a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите число b:");
+            double? b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("1. Сумма");
+            Console.WriteLine("2. Вычитание");
+            Console.WriteLine("3. Умножение");
+            Console.WriteLine("4. Деление");
+
+            Console.WriteLine("Результат: ");
+            int? c = Convert.ToInt16(Console.ReadLine());
+        }
+    }
+}
